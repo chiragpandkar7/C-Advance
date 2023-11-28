b9845c85e2228cc8c4bfce37c6d41fe33c6d65c2
chiragpandkar7 <149322622+chiragpandkar7@users.noreply.github.com>
2023-11-28 10:40:45 +0530

    Add files via upload

---
+++ Assignment4/headers/Geometry.h
@@ -0,0 +1,15 @@
+#include "Triangulation.h"
+
+// Geometry class representing geometric data and operations.
+class Geometry
+{
+public:
+    // Constructor for the Geometry class.
+    Geometry(/* args */);
+
+    // Destructor for the Geometry class.
+    ~Geometry();
+
+private:
+    Triangulation* mTriangulation; // Pointer to a Triangulation object for handling triangulation operations.
+};
