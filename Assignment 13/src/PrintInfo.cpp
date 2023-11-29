5afc78201488ddabe79042cbbed68e62e446f9c3
chiragpandkar7 <149322622+chiragpandkar7@users.noreply.github.com>
2023-11-29 11:04:48 +0530

    Delete Assignment 13 directory

--- Assignment 13/src/PrintInfo.cpp
+++
@@ -1,6 +0,0 @@
-#include "../headers/PrintInfo.h"
-
-void fileNotFoundError()
-{
-    printf("No such file found !!!");
-}
--- Assignment 13/src/print.cpp
+++
@@ -1,5 +0,0 @@
-#include "../headers/print.h"
-void print()
-{
-    printf("File not found");
-}
